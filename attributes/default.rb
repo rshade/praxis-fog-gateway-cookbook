3f059014a0f9294a7dfb7b994642d01ade66c4f8
Richard Shade <rshade@rightscale.com>
2015-03-06 10:21:27 -0500

    adding bluepill and fog creds

---
+++ attributes/default.rb
@@ -0,0 +1,1 @@
+node.default['praxis_fog_gateway_cookbook']['fog_creds'] = "{}"
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,13 +16,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require 'json'
+praxis_dir='/opt/praxis-fog-gateway'
 node.default['rvm']['default_ruby'] = "2.1.5"
 node.default['rvm']['user_default_ruby'] = "2.1.5"
 include_recipe "rvm::system"
 include_recipe "rvm::gem_package"
+include_recipe "bluepill::default"
 
 gem "unicorn" do
   action :install
 end
 
+gem "bundler" do
+  action :install
+end
 
+git praxis_dir do
+  repository 'git://github.com/rshade/praxis-fog-gateway.git'
+  revision node['praxis_fog_gateway_cookbook']['git_revision']
+  action :sync
+end
+
+bash "bundle install" do
+  cwd praxis_dir
+  code <<-EOF
+    bundle install
+  EOF
+end
+
+template "#{praxis_dir}/config/.fog.yml" do
+  source "fog.yml.erb"
+  owner "root"
+  group "root"
+  mode "0600"
+  variables({
+    :creds => JSON.parse(node['praxis_fog_gateway_cookbook']['fog_creds'])
+  })
+  action :create
+end
+
+directory "#{praxis_dir}/logs" do
+  owner "root"
+  group "root"
+  owner "0755"
+  recursive true
+  action :create
+end
+
+directory "#{praxis_dir}/tmp/pids" do
+  owner "root"
+  group "root"
+  owner "0755"
+  recursive true
+  action :create
+end
+
+template '/etc/bluepill/praxis-fog-gateway.pill' do
+    source 'praxis-fog-gateway.pill.erb'
+end
+
+bluepill_service 'praxis-fog-gateway' do
+  action [:enable, :load, :start]
+end
